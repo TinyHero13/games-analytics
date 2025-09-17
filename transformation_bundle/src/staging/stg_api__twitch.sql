42075aac1e04c981274aea452f3b947396e32c21
Yasmim Abrahao <y_abrahao@id.uff.br>
2025-09-17 10:38:32 -0300

    fix: wrong type on column

---
+++ transformation_bundle/src/staging/stg_api__twitch.sql
@@ -0,0 +1,21 @@
+create or refresh materialized view stg_twitch (
+    twitch_id integer 
+    , name string
+    , box_art_url string
+    , igdb_id integer 
+    , ingestion_timestamp timestamp
+)
+as
+with
+    source_data as (
+        select *
+        from (games_analytics.raw.twitch)
+    )
+
+select 
+    cast(id as integer) as twitch_id
+    , name
+    , box_art_url
+    , cast(igdb_id as integer) as igdb_id
+    , ingestion_timestamp
+from source_data;
