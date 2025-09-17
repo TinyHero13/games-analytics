c60384e44ca24a15fdaa0f3278f3d7a0638062b6
Yasmim Abrahao <y_abrahao@id.uff.br>
2025-09-17 10:38:02 -0300

    feat: add stg_twitch_tracker sql

---
+++ transformation_bundle/src/staging/stg_api__twitch_tracker.sql
@@ -0,0 +1,23 @@
+create or refresh materialized view stg_twitch (
+    twitch_tracker_id integer 
+    , average_viewers integer
+    , average_channels integer
+    , rank integer 
+    , hours_watched integer
+    , ingestion_timestamp timestamp
+)
+as
+with
+    source_data as (
+        select *
+        from (games_analytics.raw.twitch_tracker)
+    )
+
+select 
+    cast(id as integer) as twitch_tracker_id
+    , avg_viewers
+    , avg_channels
+    , rank
+    , hours_watched
+    , ingestion_timestamp
+from source_data;
