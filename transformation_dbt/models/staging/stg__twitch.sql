d64b7c53e1e7aa94f5bd167c53c041b605c1bb4d
Yasmim Abrahao <y_abrahao@id.uff.br>
2025-09-18 13:20:28 -0300

    fix: add try)cast for igdb_id column

--- transformation_dbt/models/staging/stg__twitch.sql
+++ transformation_dbt/models/staging/stg__twitch.sql
@@ -4,8 +4,8 @@ with
             cast(id as int) as twitch_id
             , name as game_name
             , box_art_url
-            , cast(igdb_id as int) as igdb_id
-            , ingestion_timestamp
+            , try_cast(igdb_id as int) as igdb_id
+            , ingestion_date
         from {{ source('games_sources', 'twitch') }}
     )
 
